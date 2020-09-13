7a358e97cd4b4a03fea1da9930491ddb7521c560
arafat_ratul <yeasirarafat.ratul@gmail.com>
2020-09-14 01:34:07 +0600

    products

--- SiteSettings/views.py
+++ SiteSettings/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.conf import settings
 from .models import Setting, Slider
 from django.views.generic.list import ListView
+from store.models import Product
 # Create your views here.
 
 
@@ -19,9 +20,10 @@ class About(ListView):
 def Home(request):
     setting = Setting.objects.get(id=1)
     sliding = Slider.objects.all
-
+    latest_products = Product.objects.all().order_by('-id')[:8]
     context = {'setting': setting,
                'sliding': sliding,
+               'latest': latest_products,
 
                }
     return render(request, 'home.html', context)
--- store/models.py
+++ store/models.py
@@ -23,6 +23,7 @@ class Product(models.Model):
     new_price = models.DecimalField(decimal_places=2, max_digits=10, default=0)
     old_price = models.DecimalField(decimal_places=2, max_digits=10, default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    color = models.CharField(max_length=20, blank=True, null=True)
     description = models.CharField(
         max_length=200, default='', null=True, blank=True)
     image = models.ImageField(blank=True, upload_to='products/')
