76a1761ad4cd8b575db726a5b4967ebcaf081f84
arafat_ratul <yeasirarafat.ratul@gmail.com>
2020-09-21 23:24:24 +0600

    old_price_added

--- store/models.py
+++ store/models.py
@@ -21,7 +21,8 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=200)
     new_price = models.DecimalField(decimal_places=2, max_digits=10, default=0)
-    old_price = models.DecimalField(decimal_places=2, max_digits=10, default=0)
+    old_price = models.DecimalField(
+        decimal_places=2, max_digits=10, null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     color = models.CharField(max_length=20, blank=True, null=True)
     description = models.CharField(
